84d1954f3355f284f2bf93c1289992e50943c6b6
udayrana11 <uday100519921@gmail.com>
2025-06-24 23:44:37 +0530

    logging and exception added

--- demo.py
+++ demo.py
@@ -6,14 +6,14 @@
 # logger.error("This is an error message.")
 # logger.critical("This is a critical message.")
 
+# -------------------------------------------------------
 
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-from src.logger import logging
-from src.exception import MyException
-import sys
-
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
