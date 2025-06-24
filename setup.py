4ede96f011a778a5ecedc5e4832410fa3cb5ff7a
udayrana11 <uday100519921@gmail.com>
2025-06-24 21:37:20 +0530

    updates

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Udaybhan Rana",
+    author_email="uday100519921@gmail.com",
+    packages=find_packages()
+)
